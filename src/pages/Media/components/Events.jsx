1a88c7c6bab86c6a3a2f5d961318f5d3b9e43f6f
Arcee69 <olawalearcee@gmail.com>
2025-03-14 12:23:09 +0100

    Media Responsiveness

--- src/pages/Media/components/Events.jsx
+++ src/pages/Media/components/Events.jsx
@@ -71,37 +71,36 @@ const Events = () => {
             data-aos-duration="1000" 
             data-aos-easing="linear"
         >
-            <div className='flex items-center justify-between absolute top-44 z-10 -inset-x-4'>
+            <div className='hidden lg:flex items-center justify-between absolute top-44 z-10 -inset-x-4'>
                 <img src={Left} alt='Left' className='' />
                 <img src={Right} alt='Right' className='' />
             </div>
 
             <div className='flex flex-col items-center pt-[45px] pb-[80px]'>
-                <div className='flex flex-col items-center gap-4 w-[688px] mx-auto'>
+                <div className='flex flex-col items-center gap-4 lg:w-[688px] mx-auto'>
                     <div className='w-[211px] h-[28px] rounded-[6px] gap-1 flex items-center justify-center bg-[#FDF2E9]'>
                         <img src={Stars} alt='Stars' className='w-[13px] h-[13px]' />
                         <p className='text-xs font-hanken text-[#E78020]'>Empowering 6M+ Nigerians</p>
                     </div>
                     <div className='flex flex-col gap-6'>
-                        <p className='font-hanken capitalize text-[56px] text-center font-medium text-[#FFFFFF] leading-[56px]'>
+                        <p className='font-hanken capitalize text-[32px] leading-[32px] lg:text-[56px] text-center font-medium text-[#FFFFFF] lg:leading-[56px]'>
                             A picture is worth more
                         </p>
-                        <p className='font-hanken text-[20px] leading-[30px] text-[#FFFFFF] text-center'>
+                        <p className='font-hanken text-base lg:text-[20px] leading-[30px] text-[#FFFFFF] text-center'>
                             Explore our galleries to see pictures of past seminars, trainings, meetings and other events
                         </p>
                     </div>
-                    <div className='flex items-center gap-[17px]'>
+                    <div className='hidden lg:flex items-center gap-[17px]'>
                         <button
                             className='w-[180px] h-[56px] flex items-center justify-center bg-[#fff] rounded-lg'
                         >
-                            <p className='text-[#F99650] font-medium text-base leading-7' >Get Started</p>
+                            <p className='text-[#F99650] font-medium text-base leading-7'>Get Started</p>
                         </button>
                         <button
                             className='w-[182px] h-[54px] border-[#FFFFFF] border flex items-center justify-center rounded-lg'
                         >
                             <p className='text-[#FFFFFF] font-medium text-base leading-7'>Learn More</p>
                         </button>
-
                     </div>
                 </div>
             </div>
@@ -111,11 +110,11 @@ const Events = () => {
             data-aos="fade-up" 
             data-aos-duration="1000" 
             data-aos-easing="linear"
-            className='py-[96px] px-[80px] flex flex-col items-center gap-[64px]'
+            className='py-[96px] px-5 lg:px-[80px] flex flex-col items-center gap-[64px]'
         >
-            <p className='font-hanken text-[48px] font-medium text-[#101828]'>Events Gallery</p>
+            <p className='font-hanken text-[32px] lg:text-[48px] font-medium text-[#101828]'>Events Gallery</p>
 
-            <div className='grid grid-cols-3 gap-6'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {currentGallery.map((event, index) => (
                     <div key={index} className='relative group overflow-hidden rounded-lg'>
                         <img src={event.image} alt={event.title} className='w-full h-auto object-cover' />
@@ -165,35 +164,35 @@ const Events = () => {
         </section>
 
         <section
-            className='w-full px-[80px] py-[96px] h-[512px] overflow-hidden'
+            className='w-full px-5 lg:px-[80px] py-[96px] lg:h-[512px] overflow-hidden'
             data-aos="fade-up" 
             data-aos-duration="1000"
             data-aos-easing="linear"
         >
             <div
-            className='flex items-center justify-center gap-[64px]'
+                className='flex flex-col lg:flex-row items-center justify-center gap-[64px]'
             >
-            <div className='w-[576px] flex flex-col gap-6'>
-                <p className='font-medium font-hanken text-[48px] leading-[60px]'>
-                    Bank On the Go – Anytime, Anywhere!
-                </p>
-                <p className='text-[#667085] text-[20px] font-nunito leading-[30px] w-[480px]'>
-                Download LAPO's mobile app for seamless banking, savings, 
-                and loans—all at your fingertips.
-                </p>
-                <div className='flex items-center gap-4'>
-                    <img src={Apple} alt='Apple' className='w-[120px] cursor-pointer h-[40px]' onClick={() => window.open("https://apps.apple.com/ng/app/lapo-mobile-app/id6448030325", "_blank")} />
-                    <img src={Google} alt='Google' className='w-[120px] cursor-pointer h-[40px]' onClick={() => window.open("https://play.google.com/store/apps/details?id=com.digicore.lapo", "_blank")}/>
+                <div className='lg:w-[576px] flex flex-col gap-6'>
+                    <p className='font-medium font-hanken text-[32px] lg:text-[48px] leading-[60px]'>
+                        Bank On the Go – Anytime, Anywhere!
+                    </p>
+                    <p className='text-[#667085] text-[20px] font-nunito leading-[30px] lg:w-[480px]'>
+                        Download LAPO's mobile app for seamless banking, savings, 
+                        and loans—all at your fingertips.
+                    </p>
+                    <div className='flex items-center gap-4'>
+                        <img src={Apple} alt='Apple' className='w-[120px] cursor-pointer h-[40px]' onClick={() => window.open("https://apps.apple.com/ng/app/lapo-mobile-app/id6448030325", "_blank")} />
+                        <img src={Google} alt='Google' className='w-[120px] cursor-pointer h-[40px]' onClick={() => window.open("https://play.google.com/store/apps/details?id=com.digicore.lapo", "_blank")}/>
+                    </div>
                 </div>
-            </div>
-            <div className='bg-[#F3F5F4] w-[576px] relative overflow-hidden flex items-center rounded-tl-[30px] rounded-tr-[30px]'>
-                <div className='w-[274px] relative left-10 -bottom-20'>
-                <img src={BackScreen} alt='BackScreen' className='' />
+                <div className='bg-[#F3F5F4] w-full lg:w-[576px] h-[512px] lg:h-auto relative overflow-hidden flex items-center rounded-tl-[30px] rounded-tr-[30px]'>
+                    <div className='w-[274px] hidden lg:block lg:relative left-10 -bottom-20'>
+                        <img src={BackScreen} alt='BackScreen' className='' />
+                    </div>
+                    <div className='w-[281px] mx-auto relative lg:right-4 -bottom-10 z-10'>
+                        <img src={FrontScreen} alt='FrontScreen' className=''  />
+                    </div>
                 </div>
-                <div className='w-[281px] relative right-4 -bottom-10 z-10'>
-                <img src={FrontScreen} alt='FrontScreen' className=''  />
-                </div>
-            </div>
             </div>
         </section>
     </div>
--- src/pages/Media/components/Videos.jsx
+++ src/pages/Media/components/Videos.jsx
@@ -23,26 +23,26 @@ const Videos = () => {
             data-aos-duration="1000" 
             data-aos-easing="linear"
         >
-            <div className='flex items-center justify-between absolute top-44 z-10 -inset-x-4'>
+            <div className='hidden lg:flex items-center justify-between absolute top-44 z-10 -inset-x-4'>
                 <img src={Left} alt='Left' className='' />
                 <img src={Right} alt='Right' className='' />
             </div>
 
             <div className='flex flex-col items-center pt-[45px] pb-[80px]'>
-                <div className='flex flex-col items-center gap-4 w-[688px] mx-auto'>
+                <div className='flex flex-col items-center gap-4 lg:w-[688px] mx-auto'>
                     <div className='w-[211px] h-[28px] rounded-[6px] gap-1 flex items-center justify-center bg-[#FDF2E9]'>
                         <img src={Stars} alt='Stars' className='w-[13px] h-[13px]' />
                         <p className='text-xs font-hanken text-[#E78020]'>Empowering 6M+ Nigerians</p>
                     </div>
                     <div className='flex flex-col gap-6'>
-                        <p className='font-hanken capitalize text-[56px] text-center font-medium text-[#FFFFFF] leading-[56px]'>
+                        <p className='font-hanken capitalize text-[32px] leading-[32px] lg:text-[56px] text-center font-medium text-[#FFFFFF] lg:leading-[56px]'>
                             A video is worth more
                         </p>
-                        <p className='font-hanken text-[20px] leading-[30px] text-[#FFFFFF] text-center'>
+                        <p className='font-hanken text-base lg:text-[20px] leading-[30px] text-[#FFFFFF] text-center'>
                             Explore our galleries to see videos of past seminars, trainings, meetings and other events
                         </p>
                     </div>
-                    <div className='flex items-center gap-[17px]'>
+                    <div className='hidden lg:flex items-center gap-[17px]'>
                         <button
                             className='w-[180px] h-[56px] flex items-center justify-center bg-[#fff] rounded-lg'
                         >
@@ -63,11 +63,11 @@ const Videos = () => {
             data-aos="fade-up" 
             data-aos-duration="1000" 
             data-aos-easing="linear"
-            className='py-[96px] px-[80px] flex flex-col items-center gap-[64px]'
+            className='py-[96px] px-5 lg:px-[80px] flex flex-col items-center gap-[64px]'
         >
-            <p className='font-hanken text-[48px] font-medium text-[#101828]'>Videos Gallery</p>
+            <p className='font-hanken text-[32px] lg:text-[48px] font-medium text-[#101828]'>Videos Gallery</p>
 
-            <div className={`${videosPost?.length > 0 ? "grid grid-cols-3 gap-6" :  "flex items-center justify-center"}`}>
+            <div className={`${videosPost?.length > 0 ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" :  "flex items-center justify-center"}`}>
                 {loading ? 
                     <p className='text-2xl text-[#000] text-center font-semibold'>Loading Videos...</p> :
                     videosPost.length > 0 ?
