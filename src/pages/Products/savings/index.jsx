dc2f8d097891a45c37c09f8a14cf10dbcf4e212a
Arcee69 <olawalearcee@gmail.com>
2025-03-17 12:36:41 +0100

    Hot Fix

--- src/pages/Products/savings/index.jsx
+++ src/pages/Products/savings/index.jsx
@@ -52,7 +52,7 @@ const Savings = () => {
     <div className='w-full'>
 
         <section
-            className='bg-[#00984C] h-auto pt-[100px] relative overflow-hidden'
+            className='bg-[#00984C] h-auto pt-[120px] relative overflow-hidden'
             data-aos="fade-up" 
             data-aos-duration="1000" 
             data-aos-easing="linear"
